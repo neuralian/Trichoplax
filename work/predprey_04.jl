fa93007086e4cd5f9e4d3197e153254673660cba
Mike Paulin <neuralian@gmail.com>
2020-08-23 12:10:07 +1200

    Moving predator and prey
    preprey_04.jl

--- work/predprey_04.jl
+++ work/predprey_04.jl
@@ -4,7 +4,8 @@
 using Makie
 
 sceneWidth  = 5000.0
-sceneOffset = 1000.0
+sceneOffset = sceneWidth / 2.0
+matRadius2  = (sceneWidth / 2.0)^2
 sceneLimits = FRect(-sceneOffset, -sceneOffset, sceneWidth, sceneWidth)
 
 predatorRadius = 500.   # body radius
@@ -12,7 +13,7 @@ preyRadius = 400.
 c = 20.   # cell radius
 
 preyLocation = (0.0, 0.0)
-predatorLocation = (3000.0, 3000.0)
+predatorLocation = (2000.0, 2000.0)
 
 
 
@@ -28,6 +29,7 @@ bacteriaRadius = sceneWidth*3.
 bacteriaDensity = 1e-5 # bacteria /um^2
 nBacteria = Int(round(bacteriaDensity*π*bacteriaRadius^2))
 bacteriaPos = bacteriaRadius*(rand(nBacteria,2) .- 0.5)
+bacteriaSize = 5.0*rand(nBacteria)
 
 prey = poly!(scene,
        decompose(Point2f0, Circle(Point2f0(0.,0.), preyRadius)),
@@ -42,22 +44,23 @@ predator = poly!(scene,
 # plot bacteria
 bacteria = scatter!(scene,
               lift(s->(bacteriaPos[:,1], bacteriaPos[:,2]), t),
-              color = :green, markersize = 5)[end]
+              color = :green, markersize = bacteriaSize,
+              strokecolor = :green)[end]
 
 # function moveBacteria(bacteria, dx, dy)
 #
 #     bacteria
 
-N = 100
+N = 500
 #for i in 1:N
 record(scene, "test.mp4", 1:N) do i
 
     predatorStep = randn(2).*5.
     global preyStep = 0.95*preyStep + 0.05*randn(2).*100.
     global preyLocation = preyLocation .+ preyStep
-    global predatorLocation = predatorLocation .+ predatorStep
+    global predatorLocation = predatorLocation .+ predatorStep .+ preyStep
     global bacteriaPos += ones(nBacteria,1)*preyStep'
-
+    bacteria.markersize = 5.0*m.*(sum(bacteriaPos.^2,dims=2) .< matRadius2)[:]
     t[] = 2*π*i/N
     #sleep(0.001)
     #display(scene)

2003c5af68a4a4495ef0d890e1f2e0b454059ce8
Mike Paulin <neuralian@gmail.com>
2020-08-25 20:48:09 +1200

    Open probability
    TrichoplaxField open prob

--- work/TrichoplaxField_01.jl
+++ work/TrichoplaxField_01.jl
@@ -14,12 +14,24 @@ nD = Int(D-preyRadius)
 δ = 20.e-6*100.  # dipole separation 10μm in cm
 I = 0.1e-12     # dipole current 0.1pA
 
+# Johnson-Nyquist noise
+kB = 1.38e-23
+T = 300.
+Ω = 20.e6   # 20MΩ
+Δf = 1.0e3   # 1KHz
+σ = sqrt(4.0*kB*T*Δf)
+print(σ)
+
 # function computes field strength at radius r
 # μV/cm
 E_(r) = 1.0e6*2π*ρ*I*δ./r.^3
 # array holds computed field strength from edge of Trichoplax
 E = fill(0.0, nD)
 
+# channel open probability
+kT = kB*T
+v0 = -σ*log(0.1/(1.0-0.1))
+p(e) =  1.0./(1 .+ exp.(-(e.-v0)/σ))
 scene = Scene(resolution = (1000, 1000),
 limits = FRect(-5*preyRadius, -5*preyRadius, 10*preyRadius, 10*preyRadius))
 
@@ -41,13 +53,7 @@ end
 
 #display(scene)
 
-# Johnson-Nyquist noise
-kB = 1.38e-23
-T = 300.
-Ω = 20.e6   # 20MΩ
-Δf = 1.0e3   # 1KHz
-σ = sqrt(4.0*kB*T*Δf)
-print(σ)
+
 
 
 # field strength at distance from animal
@@ -61,11 +67,12 @@ Vnoisy = V + 1.0e6*σ*randn(size(V))
 
 # plot voltage (μV)
 d0 = 1
-d1 = 1000
-voltagePlot = plot(d[d0:d1]*1.0e4,Vnoisy[d0:d1], color = :lightblue)
-plot!(d[d0:d1]*1.0e4,V[d0:d1], color = :magenta, linewidth = 1)
+d1 = 2000
+noisyPlot = plot(d[d0:d1]*1.0e4,Vnoisy[d0:d1], color = :lightblue)
+voltagePlot =plot!(d[d0:d1]*1.0e4,V[d0:d1], color = :magenta, linewidth = 1)
 
-voltagePlot[Axis][:names][:axisnames] = ("μm from edge", "μV")
-
+noisyPlot[Axis][:names][:axisnames] = ("μm from edge", "μV")
+openPlot = plot!(d[d0:d1]*1.0e4,.04*p(V[d0:d1]*1e-6), color = :blue)
+plot!([d[d0], d[d1]]*1.0e4,.04*p(0)*[1,1], color = :red)
 display(voltagePlot)
 title(voltagePlot, "Voltage across receptor")

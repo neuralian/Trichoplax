b458a0ac9047e2f80dda108a45944ed87e74bdf7
Mike Paulin <neuralian@gmail.com>
2020-08-31 15:51:57 +1200

    likelihood contours
    Starting to draw contours of likelihood functions

---
+++ work/TrichoplaxField_02.jl
@@ -0,0 +1,85 @@
+using Makie
+
+
+sourceRadius= 400.   # body radius
+sourceMargin = 100.   # width of margin
+cellDiam = 20.   # cell radius
+maxRange = 5.0e3 # max range (from centre) in μm
+gridSize = 10  # grid spacing for field evaluation
+
+nD = Int(maxRange-sourceRadius)
+
+# source
+ρ = 25.0   # Resisitivity of seawater 25Ω.cm
+δ = 20.e-6*100.  # dipole separation 10μm in cm
+I = 0.1e-12     # dipole current 0.1pA
+
+# Johnson-Nyquist noise
+kB = 1.38e-23
+T = 300.
+Ω = 20.e6   # 20MΩ
+Δf = 1.0e3   # 1KHz
+σ = sqrt(4.0*kB*T*Δf)
+print(σ)
+
+# function computes field strength at radius r from dipole source
+# μV/cm
+E_(r) = 1.0e6*2π*ρ*I*δ./r.^3
+# array holds computed field strength from edge of Trichoplax
+E = fill(0.0, nD)
+
+# channel open probability
+kT = kB*T
+v0 = -σ*log(0.1/(1.0-0.1))
+p(e) =  1.0./(1 .+ exp.(-(e.-v0)/σ))
+scene = Scene(resolution = (1000, 1000),
+limits = FRect(-5*sourceRadius, -5*sourceRadius, 10*sourceRadius, 10*sourceRadius))
+
+body = poly!(scene, decompose(Point2f0, Circle(Point2f0(0,0), sourceRadius)),
+            color = :lightgray, strokewidth = 1, strokecolor = :black)
+
+# fill body with layers of cells
+# compute field strength at distance d from edge of body
+function fieldStrength(sourceRadius, sourceMargin, nD, E)
+  for a in cellDiam:cellDiam:(sourceRadius-cellDiam)
+    n = round(2π*a/cellDiam)
+    x = [ a*cos(2π*i/n) for i in 1:n]
+    y = [ a*sin(2π*i/n) for i in 1:n]
+    for d in 1:nD
+      r = sqrt.(((d.+sourceRadius.-x).^2 + y.^2)).*1.0e-4
+      E[d] = E[d] + sum(E_(r))
+    end
+    #scatter!(x, y, markersize = cellDiam/16)
+  end
+  return E
+end
+
+E = fieldStrength(sourceRadius, sourceMargin, nD, E)
+#display(scene)
+
+function pOpen(d, V)
+   i = Int(round(d/gridSize))
+   return i < length(E)-1 ? p(E[i+1]) : 0.0
+ end
+
+
+# field strength at distance from animal
+d = (1:nD).*1.0e-4
+#plt = lines(d, E, strokewidth = 0.1, markersize = 0)
+
+# voltage μV
+V = cumsum(E)*1.0e-4
+V = V[end].-V
+Vnoisy = V + 1.0e6*σ*randn(size(V))
+
+# plot voltage (μV)
+d0 = 1
+d1 = 2000
+noisyPlot = lines(d[d0:d1]*1.0e4,Vnoisy[d0:d1], color = :lightblue, linewidth=1)
+voltagePlot =lines!(d[d0:d1]*1.0e4,V[d0:d1], color = :magenta, linewidth = 1)
+
+noisyPlot[Axis][:names][:axisnames] = ("μm from edge", "μV")
+openPlot = lines!(d[d0:d1]*1.0e4,.04*p(V[d0:d1]*1e-6), color = :blue, linewidth=1)
+lines!([d[d0], d[d1]]*1.0e4,.04*p(0)*[1,1], color = :red, linewidth=1)
+display(voltagePlot)
+title(voltagePlot, "Voltage across receptor")
--- work/makietest2.jl
+++ work/makietest2.jl
@@ -1,18 +1,36 @@
 using Makie
+#using Unitful
+
+# R = 100.0u"μm"   # body radius
+# c =  20.0u"μm"   # cell radius
+# D =   0.5u"mm" # max range
+# dD =  5.0u"μm"
+#
+#
+# I = 1.5u"nA"  # current per cell
+# ρ = 25.0u"Ω*cm"    # resistivity of seawater 25 Ohm.cm
+# h = 10u"μm"     # 10 micron dipole separation
+
+R = 500.0   # body radius
+c =  20.0   # cell radius
+D =   1500.0 # max range
+dD =  5.0
 
 
-R = 500.   # body radius
-c = 20.   # cell radius
-D = 1000. # max range
-dD = 1.
+I = 1.5  # current per cell
+ρ = 25.0    # resistivity of seawater 25 Ohm.cm
+h = 10    # 10 micron dipole separation
 
-nD = Int(D-R)
+nD = Int(round(D-R)/dD)
 F = fill(0.0, nD)
 
-scene = Scene(resolution = (1000, 1000), limits = FRect(-2*R, -2*R, 4*R, 4*R))
+# sceneD = ustrip(D)
+# sceneR = ustrip(R)
+# scene = Scene(resolution = (1000, 1000),
+#         limits = FRect(-sceneD, -sceneD, 2*sceneD, 2*sceneD))
 
-body = poly!(scene, decompose(Point2f0, Circle(Point2f0(0,0), R)),
-            color = :lightgray, strokewidth = 1, strokecolor = :black)
+#body = poly!(scene, decompose(Point2f0, Circle(Point2f0(0,0), sceneR)),
+#            color = :lightgray, strokewidth = 1, strokecolor = :black)
 
   for r in c:c:(R-c)
     n = round(2π*r/c)
@@ -24,9 +42,14 @@ body = poly!(scene, decompose(Point2f0, Circle(Point2f0(0,0), R)),
       for d in 1:nD
       F[d] = F[d] + ((d+R-x)^2 + y^2)^(-3.0/2.0)
 
-      #scatter!([], [], markersize = c/16)
+#      scatter!([x], [y], markersize = c/16)
     end
   end
 end
-plot(1:nD, F)
-#display(scene)
+
+# alt display
+scene = Scene(); #(limits = FRect(0.0, 0.0, D-R, maximum(F)))
+plot!((1:nD).*dD, F)
+
+1+1
+display(scene)
